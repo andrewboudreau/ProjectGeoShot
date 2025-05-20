35325b0637f840ede17813e5991d3a6bba3790d5
= <andrewboudreau@gmail.com>
2025-05-19 19:08:13 -0500

    remove

--- src/Web/Pages/Index.cshtml.cs
+++ src/Web/Pages/Index.cshtml.cs
@@ -6,7 +6,6 @@ namespace ProjectGeoShot.Web.Pages
     public class IndexModel : PageModel
     {
         [BindProperty(SupportsGet = true)]
-        // Default to 3647 N Saint Louis Ave, Chicago IL 60618
         public double Lat { get; set; } = 41.9464;
 
         [BindProperty(SupportsGet = true)]
