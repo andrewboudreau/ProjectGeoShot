7ea02044cf55cc6cceeb7e908fd7b3ddda25a336
= <andrewboudreau@gmail.com>
2025-05-24 14:48:44 -0500

    Update SharedTools.Web and enhance module loading
    - Updated `SharedTools.Web` package version to `0.0.10` in project files.
    - Added new using directives in `Program.cs` and modified version handling.
    - Introduced `GetLatestReleaseVersionAsync` for fetching release versions from GitHub.
    - Created `WebModuleExtensions2` class for loading web modules from remote URLs, including dependency management and logging.
    - Added `UseWebModules` method to configure web modules in the application.
    - Implemented `TryDownloadFileAsync` for robust file downloading with error handling.

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,12 +1,17 @@
 using SharedTools.Web.Modules;
 
+using System.Net;
+using System.Text.RegularExpressions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddMemoryCache()
     .AddRazorPages();
 
-var version = "0.1.7";
+var release = await GetLatestReleaseVersionAsync("https://github.com/andrewboudreau/ProjectGeoShot/releases/latest");
+var version = release.Version;
+
 await builder.AddWebModules([
     $"https://github.com/andrewboudreau/ProjectGeoShot/releases/download/v{version}/ProjectGeoShot.Game-{version}.dll"
 ]);
@@ -19,3 +24,28 @@ app.MapRazorPages();
 app.UseWebModules();
 
 app.Run();
+
+
+static async Task<(string Version, Uri ReleaseUrl)> GetLatestReleaseVersionAsync(string latestUrl)
+{
+    // Create a handler to prevent auto-redirect
+    var handler = new HttpClientHandler { AllowAutoRedirect = false };
+    using var httpClient = new HttpClient(handler);
+
+    var response = await httpClient.GetAsync(latestUrl);
+    if (response.StatusCode == HttpStatusCode.Redirect || response.StatusCode == HttpStatusCode.MovedPermanently)
+    {
+        var redirectUrl = response.Headers.Location?.ToString()
+            ?? throw new InvalidOperationException($"latestUrl returned HttpStatusCode={response.StatusCode} but location header is empty. Url=\"{latestUrl}\"");
+
+        var match = Regex.Match(redirectUrl, @"tag/v(?<version>\d+\.\d+\.\d+)");
+        if (match.Success)
+        {
+            return (Version: match.Groups["version"].Value, ReleaseUrl: new Uri(redirectUrl));
+        }
+
+        throw new InvalidOperationException($"latestUrl did redirect but couldn't parse the version from. Redirect=\"{redirectUrl}\" Url=\"{latestUrl}\"");
+    }
+
+    throw new InvalidOperationException($"latestUrl returned HttpStatusCode={response.StatusCode}. Url=\"{latestUrl}\"");
+}
